542cdc6dbfc9e890e44804ff6fd90db575033468
hannes malter <hannesmalter1234@gmail.com>
2023-05-24 12:35:11 +0300

    change Service to services

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('Service', ServiceController::class)
+Route::resource('services', ServiceController::class)
     ->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
